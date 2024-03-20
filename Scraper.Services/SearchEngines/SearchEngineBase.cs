8296e9bef362c8adecabad2c1413692e0b39ab93
Jemzz <tolujemilua@hotmail.co.uk>
2024-03-20 11:35:38 +0000

    Added comment

--- Scraper.Services/SearchEngines/SearchEngineBase.cs
+++ Scraper.Services/SearchEngines/SearchEngineBase.cs
@@ -4,6 +4,7 @@
     {
         private readonly IHttpClientFactory _httpClient = httpClient;
 
+        // Used a class/virtual method as opposed to an interface because not every engine will have an implementation
         internal virtual HttpClient HandleCustomClientHeader()
         {
             var client = _httpClient.CreateClient();
